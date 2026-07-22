7537db249ec7ee70a760660c2394d83a68b44d33
hexlink-org <hexlink.team@gmail.com>
2026-07-22 15:51:53 +0100

    Fix edit url

--- hexlink-docs/docusaurus.config.ts
+++ hexlink-docs/docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/HexlinkSocial/hexlink-docs',
+            'https://github.com/HexlinkSocial/hexlink-docs/edit/main/hexlink-docs/docs',
         },
         blog: {
           showReadingTime: true,
