c9bc47993f32be9f1771669b2ee38d4559011809
hexlink-org <hexlink.team@gmail.com>
2026-07-22 11:49:44 +0100

    Customise home page

--- hexlink-docs/src/components/HomepageFeatures/index.tsx
+++ hexlink-docs/src/components/HomepageFeatures/index.tsx
@@ -1,57 +1,73 @@
 import type {ReactNode} from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
+  link: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Tips for Community Members',
+    link: '/docs/category/tips-for-community-members',
     Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Guides, tips, and resources to help community members get the most out of the platform.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Tips for Community Runners',
+    link: '/docs/category/tips-for-community-runners',
     Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Best practices, management strategies, and tools for organizing and running communities.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Get Involved',
+    link: '/docs/category/get-involved',
     Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Learn how you can participate, contribute, and collaborate with our growing community.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function FeatureIcon({Svg}: {Svg: FeatureItem['Svg']}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={styles.iconWrapper}>
+      <Svg className={styles.featureSvg} role="img" />
+    </div>
+  );
+}
+
+function FeatureCard({title, link, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4 margin-bottom--lg')}>
+      <Link to={link} className={styles.cardLink}>
+        <div className={clsx('card', styles.featureCard)}>
+          <div className="card__header text--center">
+            <FeatureIcon Svg={Svg} />
+            <Heading as="h3" className={styles.featureTitle}>
+              {title}
+            </Heading>
+          </div>
+          <div className="card__body text--center">
+            <p className={styles.featureDescription}>{description}</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
@@ -62,7 +78,7 @@ export default function HomepageFeatures(): ReactNode {
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <FeatureCard key={idx} {...props} />
           ))}
         </div>
       </div>
